2f3ec4d350c3aa16184f888da3ea331e7a87c983
Yasin Sutoglu <ysutoglu@gmail.com>
2022-09-16 15:06:55 +0300

    done

--- app.js
+++ app.js
@@ -6,38 +6,44 @@ const taskDiv = document.querySelector(".tasks");
 
 
 
-//Events
+//EVENTS
 addTask.addEventListener("click" , ()=>{    
    
      let task = document.createElement("div");
      task.className = "task";
      task.innerHTML = `<p>${inputTask.value}</p>
                         <div>
-                            <button class="check"><i class="fa-solid fa-circle-check"></i></button>
+                            <button class="checked"><i class="fa-solid fa-circle-check"></i></button>
+                            <button class="unchecked"><i class="fa-solid fa-check-double"></i></button>
                             <button class="trash"><i class="fa-solid fa-trash-can"></i></button>
                         </div>
                     `;
 
     if(!inputTask.value){
         alert("Enter Something")
+    }else{
+        taskDiv.appendChild(task);
     }
-         taskDiv.appendChild(task);
-
 
     inputTask.value = "";
+    inputTask.focus();
 
-     const check = document.querySelector(".check");
-     const trash = document.querySelector(".trash");
+     const checkeds = document.querySelectorAll(".checked");
+     const uncheckeds = document.querySelectorAll(".unchecked")
+     const trashes = document.querySelectorAll(".trash");
 
-    check.addEventListener("click", ()=>{
-        check.parentElement.previousElementSibling.classList.toggle("linethrough");
-    })
-    
-    trash.addEventListener("click" , (e)=>{
+    checkeds.forEach(el => el.addEventListener("click", ()=>{
+         el.parentElement.parentElement.firstElementChild.classList.add("linethrough");
+    }));
+
+    uncheckeds.forEach((c) => c.addEventListener("click", () => {
+        c.parentElement.previousElementSibling.classList.remove("linethrough");
+      })
+    );
+
+    trashes.forEach(el => el.addEventListener("click", (e)=>{
         e.target.parentElement.parentElement.parentElement.remove();
-    })
-    
-     
+    }))     
 
 });
 
@@ -46,6 +52,10 @@ inputTask.addEventListener("keydown", (e)=>{
     if(e.code == "Enter"){
         addTask.click();
     }
+})
+
+window.addEventListener("load", ()=>{
+    inputTask.focus();
 })
 
 
